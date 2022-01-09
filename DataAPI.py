467c6d96f7717396ba644acaa2b03e916d65363c
lomofille <97266477+lomofille@users.noreply.github.com>
2022-01-08 17:39:48 -0800

    Update DataAPI.py

--- DataAPI.py
+++ DataAPI.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
+pip install bls        #Bureau of Labor Statistics API
+pip install yfinance   #Yahoo! Finance
 
-# In[ ]:
+import yfinance as yf
+import datetime as dt
+import bls
 
 
 # Download market data from Yahoo! Finance API
-
-get_ipython().system('pip install yfinance')
-import yfinance as yf
-import datetime as dt
-
 # Load tickers
 snp = yf.Ticker("SNP")
 b = yf.Ticker('BTC-USD')
@@ -18,7 +15,6 @@ g = yf.Ticker('GC=F')
 # Define start and end date for data pull
 start = dt.datetime(2016,7,1)
 end = dt.datetime(2021,11,30)
-
 
 # Write data to csv files
 sandp = yf.download('^GSPC', start, end)
@@ -31,23 +27,13 @@ gold = btc = yf.download('GC=F', start, end)
 gold.to_csv('gold_datafile.csv')
 
 
-# In[ ]:
-
-
 # Download market data from BLS API
 # Note this is monthly data
 
-get_ipython().system('pip install bls')
-
-import bls
 inflation = bls.get_series('CUUR0000SA0', 2016, 2021)
 inflation.head()
 
 inflation.to_csv("inf_datafile.csv")
-
-
-# In[ ]:
-
 
 
 
